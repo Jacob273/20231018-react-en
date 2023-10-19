067edd96f36231bc310b539f27bc14ad1dc4ab99
Jabi <michal.jabi@gmail.com>
2023-10-19 12:01:04 +0200

    feat(auction-portal): Toggle button works in MainMenu

--- auction-portal/src/components/MainMenu.jsx
+++ auction-portal/src/components/MainMenu.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 
 export function MainMenu() {
 
+  const [isMenuShown, setMenuShown] = useState(false);
+  const standardCollapseClasses = "collapse navbar-collapse";
+  const computedClasses =  isMenuShown ? standardCollapseClasses + ' show' : standardCollapseClasses; 
+
+    // setMenuShown(true) // WARN: that will cause infinite loop of re-render
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 px-3">
-        <button className="navbar-toggler" type="button">
-        <span className="navbar-toggler-icon" />
+        <button className="navbar-toggler" type="button" onClick={() => setMenuShown(!isMenuShown)}>
+            <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse show">
+        {/* <div className={"collapse navbar-collapse" + (true ? " show" : "")}> */}
+        <div className={computedClasses}>
         <ul className="navbar-nav">
             <li className="nav-item active">
                 <a className="nav-link" href="/auctions">
